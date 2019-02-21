17dc190e7c68b28dfd7cbbeab123ddd67ef536ea
oliveirandre <oliveir.andre@ua.pt>
2019-02-21 12:39:03 +0000

    setting up what to do

--- src/tp1/RepairShop.java
+++ src/tp1/RepairShop.java
@@ -12,7 +12,13 @@ package tp1;
 public class RepairShop {
     
     public static void main(String[] args) {
-        
+        /* 
+         * There are 5 different locations: park, lounge, repair area, supplier
+         * site and the outside world.
+         * We assume that there are 30 customers, 2 mechanics and 1 manager.
+         * Furthermore, there are 3 replacement vehicles and 3 distinct parts 
+         * that can be repared.
+         */
     }
     
 }

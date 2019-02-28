11863ab1c0d16fb4ad1f4c8f8cac32931916dac4
oliveirandre <oliveir.andre@ua.pt>
2019-02-28 12:38:00 +0000

    entities and shared

---
+++ src/tp1/Queue.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp1;
+
+/**
+ *
+ * @author andre and joao
+ */
+public class Queue {
+ 
+    Object[] arr = null;
+    
+    public Queue() {
+        
+    }
+    
+    public void add(Object o) {
+        
+    }
+    
+    public Object remove() {
+       return null; 
+    }
+    
+    public Object peek() {
+        return null;
+    }
+    
+    public boolean isEmpty() {
+        return true;
+    }
+}
--- src/tp1/RepairShop.java
+++ src/tp1/RepairShop.java
@@ -1,4 +1,4 @@
-package shared;
+package tp1;
 
 /**
  *

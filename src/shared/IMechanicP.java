788a269c5387adbb3818d42f21a3aacfe1851afd
jcoel <jcoel@DESKTOP-2R3HSOQ>
2019-03-14 18:43:26 +0000

    mechanic, lounge and park modifications

--- src/shared/IMechanicP.java
+++ src/shared/IMechanicP.java
@@ -1,14 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package shared;
 
 /**
  *
- * @author andre
+ * @author andre e joao
  */
 public interface IMechanicP {
-    
+    public void getVehicle(int id);
+    public void returnVehicle(int id);
 }
--- src/shared/IRepairArea.java
+++ src/shared/IRepairArea.java
@@ -7,22 +7,29 @@ import repository.Piece;
  * @author andre and joao
  */
 public interface IRepairArea {
-    public void readThePaper();
-	
-	public void startRepairProcedure();
-	public void getVehicle();
-	public void fixIt(Piece p);
-	public void returnVehicle();
-    public void getNextTask();
-	public Piece getRequiredPart();
-	
-	public boolean partAvailable(Piece p);
-	public void letManagerKnow();
-	
-	public void resumeRepairProcedure();
-	public void repairConcluded();
 
-	public void storePart();
-	
-	
+    public void readThePaper();
+
+    public void startRepairProcedure();
+
+    public int getVehicle();
+
+    public void fixIt(Piece p);
+
+    public void returnVehicle();
+
+    public void getNextTask();
+
+    public Piece getRequiredPart();
+
+    public boolean partAvailable(Piece p);
+
+    public void letManagerKnow();
+
+    public void resumeRepairProcedure();
+
+    public void repairConcluded();
+
+    public void storePart();
+
 }
